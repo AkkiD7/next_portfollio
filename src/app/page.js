75ee3a710b927af0ea59dcb4bccb08afe5e82073
Akshay Dabahde <113975806+AkkiD7@users.noreply.github.com>
2024-09-03 20:51:25 +0530

    Update page.js

--- src/app/page.js
+++ src/app/page.js
@@ -8,14 +8,19 @@ import Projects from "@/components/Projects";
 export default function Home() {
   return (
     <>
-      <div className="absolute top-0 z-[-2] h-screen w-screen bg-white bg-[radial-gradient(100%_50%_at_50%_0%,rgba(0,163,255,0.13)_0,rgba(0,163,255,0)_50%,rgba(0,163,255,0)_100%)]"></div>
-      <Navbar />
-      <div className="container mx-auto px-8 ">
-        <Hero />
-        <About />
-        <Technologies />
-        <Experiences />
-        {/* <Projects/> */}
+      <div className="overflow-x-hidden text-white antialiased ">
+        <div className="fixed top-0 -z-10 h-full w-full">
+          <div className="absolute top-0 z-[-2] h-screen w-screen bg-neutral-950 bg-[radial-gradient(ellipse_80%_80%_at_50%_-20%,rgba(120,119,198,0.3),rgba(255,255,255,0))]"></div>{" "}
+        </div>
+
+        <div className="container mx-auto px-8 ">
+          <Navbar />
+          <Hero />
+          <About />
+          <Technologies />
+          <Experiences />
+          {/* <Projects/> */}
+        </div>
       </div>
     </>
   );
