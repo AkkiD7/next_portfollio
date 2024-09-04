3e5a7e061ab22a227d33af38b040ac6cd8d6cede
Akshay Dabahde <113975806+AkkiD7@users.noreply.github.com>
2024-09-04 15:56:51 +0530

    updated

--- src/components/About.js
+++ src/components/About.js
@@ -1,31 +1,42 @@
 import { ABOUT_TEXT } from "@/data/data";
 import Image from "next/image";
 import React from "react";
+import { motion } from "framer-motion";
 
 export default function About() {
   return (
     <>
       <div className="border-b ☐ border-neutral-900 pb-4">
         <h1 className="my-20 text-center text-4xl">About Me</h1>
-      
-      <div className="flex flex-wrap">
-        <div className="w-full lg:w-1/2 lg:p-8">
-          <div className="flex items-center justify-center">
-            <Image
-              src="/img/about.jpg"
-              width={500}
-              height={500}
-              alt="about"
-              className="rounded-2xl"
-            />
-          </div>
+
+        <div className="flex flex-wrap">
+          <motion.div
+            whileInView={{ x: 0, opacity: 1 }}
+            initial={{ x: -100, opacity: 1 }}
+            transition={{ duration: 0.5 }}
+            className="w-full lg:w-1/2 lg:p-8"
+          >
+            <div className="flex items-center justify-center">
+              <Image
+                src="/img/about.jpg"
+                width={500}
+                height={500}
+                alt="about"
+                className="rounded-2xl"
+              />
+            </div>
+          </motion.div>
+          <motion.div
+            whileInView={{ x: 0, opacity: 1 }}
+            initial={{ x: 100, opacity: 1 }}
+            transition={{ duration: 0.5 }}
+            className="w-full lg:w-1/2"
+          >
+            <div className="flex justify-center lg:justify-center">
+              <p className="my-2 max-w-xl py-6">{ABOUT_TEXT}</p>
+            </div>
+          </motion.div>
         </div>
-        <div className="w-full lg:w-1/2">
-          <div className="flex justify-center lg:justify-center">
-            <p className="my-2 max-w-xl py-6">{ABOUT_TEXT}</p>
-          </div>
-        </div>
-      </div>
       </div>
     </>
   );
--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,7 +1,7 @@
 import { HERO_CONTENT } from "@/data/data";
 import Image from "next/image";
 import React from "react";
-import { motion } from "framer-motion";
+import {motion } from "framer-motion";
 
 const container = (delay) => ({
   hidden: { x: -100, opacity: 0 },
@@ -46,7 +46,12 @@ export default function Hero() {
             </div>
           </div>
           <div className="w-full lg:w-1/2 lg:p-8">
-            <div className="flex justify-center">
+            <motion.div
+              initial={{ x: 100, opacity: 0 }}
+              animate={{ x: 0, opacity: 1 }}
+              transition={{ duration: 1, delay: 1.2 }}
+              className="flex justify-center"
+            >
               <Image
                 src="/img/AkkiD7Profile.jpg"
                 width={400}
@@ -54,7 +59,7 @@ export default function Hero() {
                 alt="Picture of the author"
                 className="rounded-full"
               />
-            </div>
+            </motion.div>
           </div>
         </div>
       </div>
