e4caa0ffc9cf176c59eedcbb9218518c4e3520aa
Akshay Dabahde <113975806+AkkiD7@users.noreply.github.com>
2024-09-04 20:28:31 +0530

    Final Animations Added

--- src/components/Experiences.js
+++ src/components/Experiences.js
@@ -14,10 +14,18 @@ export default function Experiences() {
         <div>
           {EXPERIENCES.map((experience, index) => (
             <div key={index} className="mb-8 flex flex-wrap lg:justify-center">
-              <div className="w-full lg:w-1/4">
+              <motion.div
+                whileInView={{opacity:1,x:0}}
+                initial={{opacity:0,x:-100}}
+                transition={{duration:1}}
+              className="w-full lg:w-1/4">
                 <p className="mb-2 text-sm text-neutral-400">{experience.year}</p>
-              </div>
-              <div className="w-full max-w-xl lg:w-3/4">
+              </motion.div>
+              <motion.div
+                whileInView={{opacity:1,x:0}}
+                initial={{opacity:0,x:100}}
+                transition={{duration:1}}
+                 className="w-full max-w-xl lg:w-3/4">
                 <h6 className="mb-2 font-semibold">
                   {experience.role} −{" "}
                   <span className="text-sm text-purple-100">
@@ -33,7 +41,7 @@ export default function Experiences() {
                     {tech}
                   </span>
                 ))}
-              </div>
+              </motion.div>
             </div>
           ))}
         </div>
