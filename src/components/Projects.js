1962efc254c47fd23aef0ac7431b656bd50bf1c2
Akshay Dabahde <113975806+AkkiD7@users.noreply.github.com>
2024-09-05 13:37:38 +0530

    Update Projects.js

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -25,8 +25,8 @@ export default function Projects() {
             >
               <Image
                 src={project.image}
-                width={250}
-                height={300}
+                width={300}
+                height={400}
                 alt={project.title}
                 className="mb-6 rounded"
               />
@@ -36,7 +36,7 @@ export default function Projects() {
               whileInView={{ opacity: 1, x: 0 }}
               initial={{ opacity: 0, x: 100 }}
               transition={{ duration: 1 }}
-              className="w-full max-w-xl lg:w-3/4"
+              className="w-full  max-w-xl lg:w-3/4"
             >
               <h6 className="mb-2 font-semibold">{project.title}</h6>
               <p className="mb-4 text-neutral-400">{project.description}</p>
