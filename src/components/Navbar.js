40defd456cf4a8952ac2ff2dd68ca6e607a45f47
Akshay Dabahde <113975806+AkkiD7@users.noreply.github.com>
2024-09-05 13:41:52 +0530

    Update Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,3 +1,4 @@
+import Link from "next/link";
 import { FaLinkedin } from "react-icons/fa";
 import { FaXTwitter } from "react-icons/fa6";
 import { FaGithub } from "react-icons/fa";
@@ -8,19 +9,38 @@ export default function Navbar() {
     <>
       <nav className="flex items-center justify-between shadow-md mb-20">
         <div className="flex flex-shrink-0 items-center">
-          <h1
-            className=" font-extrabold text-xl
-           mx-2 "
-          >
-            akkid7.dev
-          </h1>
+          <h1 className="font-extrabold text-xl mx-2">akkid7.dev</h1>
         </div>
 
         <div className="m-8 flex items-center gap-4 text-2xl">
-          <FaLinkedin />
-          <FaXTwitter />
-          <FaGithub />
-          <FaInstagram />
+          <Link
+            href="https://www.linkedin.com/in/akkid7"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <FaLinkedin />
+          </Link>
+          <Link
+            href="https://github.com/AkkiD7"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <FaGithub />
+          </Link>
+          <Link
+            href="https://x.com/AkkiD77"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <FaXTwitter />
+          </Link>
+          <Link
+            href="https://www.instagram.com/akkid7"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <FaInstagram />
+          </Link>
         </div>
       </nav>
     </>
